2045c07e0eb710391cd50caa9b1726042543afbf
Arifa Islam <minajmiha02@gmail.com>
2024-02-19 17:17:06 +0600

    added footer

--- style.js
+++ style.js
@@ -1,3 +1,5 @@
-function  (){
-   const 
-}
+document.getElementsByID(`seat-information-1`).addEventListener(`click`,function(){
+    const ticktInformation = document.getElementById(`A1`);
+    ticktInformation.innerText = `A1`
+    // console.log(`arifa`);
+})
