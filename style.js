fbcb2752ec9e44fa50df75e3fefb1a0c0ccaaff6
Arifa Islam <minajmiha02@gmail.com>
2024-02-24 17:22:06 +0600

    added a button

--- style.js
+++ style.js
@@ -1,5 +1,85 @@
-document.getElementsByID(`seat-information-1`).addEventListener(`click`,function(){
-    const ticktInformation = document.getElementById(`A1`);
-    ticktInformation.innerText = `A1`
-    // console.log(`arifa`);
-})
+const allBtn = document.getElementsByClassName(`btn`);
+for(const btn of allBtn){
+ btn.addEventListener(`click`,function(event){
+    const name = event.target.innerText;
+    const tonMoy = document.getElementById(`Economoy`).innerText
+    const perPrice = getConvertedValue(`per-price`);
+    
+
+ const selectContainer = document.getElementById(`container`);
+ const div = document.createElement("div");
+ div.classList.add(`flex`);
+
+
+
+ const p1 = document.createElement(`p`);
+ const p2 = document.createElement(`p`);
+ const p3 = document.createElement(`p`);
+
+
+ p1.classList.add(`mx-8`);
+ p2.classList.add(`mx-8`);
+ p3.classList.add(`mx-8`);
+   
+   p1.innerText = name;
+   p2.innerText = tonMoy;
+   p3.innerText = perPrice;
+
+   div.appendChild(p1);
+   div.appendChild(p2);
+   div.appendChild(p3);
+   selectContainer.appendChild(div);
+
+   updateTotalPrice(perPrice);
+   
+  updateGrandPrice();
+    
+ })
+}
+
+
+// update badget
+
+
+
+
+function updateGrandPrice(status){
+  const totalPrice = getConvertedValue(`total-price`);
+  if (status === undefined){
+   
+    document.getElementById(`grand-price`).innerText = totalPrice;
+
+  }else{
+    const couponCode = document.getElementById(`coupon-code`).value;
+    if(couponCode === `NEW15`){
+      
+       const discounted = (totalPrice * 20 )/100;
+       
+      document.getElementById(`grand-price`).innerText = totalPrice-discounted ;
+    }else{
+      alert(`please enter valid coupon code.`)  
+    }
+  }
+}
+
+
+
+
+function updateTotalPrice(price){
+ const totalPrice = getConvertedValue(`total-price`);
+const convertedPrice = parseFloat(price);
+const sum =  totalPrice + convertedPrice;
+document.getElementById(`total-price`).innerText = sum;
+}
+ 
+ 
+
+
+
+
+
+function getConvertedValue(id){
+    const price = document.getElementById(id).innerText;
+    const convertPrice = parseFloat(price);
+    return convertPrice;
+}
